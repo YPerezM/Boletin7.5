95816bcdd9ddbd2a96bc5d99d43e564131762fc7
yperezmartinez <yperezmartinez@eniac12.danielcastelao.org>
2014-11-06 10:37:06 +0100

    Scanner

---
+++ src/boletin7/pkg5/Boletin75.java
@@ -0,0 +1,20 @@
+
+package boletin7.pkg5;
+
+public class Boletin75 {
+
+    public static void main(String[] args) {
+        float num1,num2,num3;
+        Numeros obx=new Numeros();
+        num1=obx.pedirDato();
+        num2=obx.pedirDato();
+        num3=obx.pedirDato();
+        obx.setNumero1(num1);
+        obx.setNumero2(num2);
+        obx.setNumero3(num3);
+        obx.condicion();
+    
+    }
+    
+    
+}
